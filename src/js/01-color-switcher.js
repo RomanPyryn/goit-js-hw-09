76db659264196d3746c6c80faaa7b5379b579f3f
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2022-05-13 08:25:23 +0300

    Add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,3 +3,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnStartEl = document.querySelector("button[data-start]");
+const btnStopEl = document.querySelector("button[data-stop]");
+const bodyEl = document.querySelector("body");
+
+btnStartEl.addEventListener('click', onBtnStartCklick);
+btnStopEl.addEventListener('click', onBtnStopCklick);
+
+function generatorColor() { 
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+function onBtnStartCklick() {
+  console.log('Start');
+
+  generatorColor();
+
+  btnStopEl.removeAttribute("disabled");
+  btnStartEl.setAttribute("disabled", "disabled");
+};
+
+function onBtnStopCklick() {
+  console.log('Stop');
+
+  clearInterval(timerId);
+  btnStartEl.removeAttribute('disabled');
+  btnStopEl.setAttribute("disabled", "disabled");
+};
